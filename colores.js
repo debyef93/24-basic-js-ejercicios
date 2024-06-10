8d8196d796fd3da4c2a760c6b2e2c7204ab0b0eb
deby <escuderoflores1993@gmail.com>
2024-06-10 21:47:00 +0200

    cambia violeta

--- colores.js
+++ colores.js
@@ -1,6 +1,6 @@
-var colors = ["rosa", "", "azul", "rojo"];
+var colors = ["rosa", "violeta", "azul", "rojo"];
 
-console.log(colors[0]);
-console.log(colors[1]);
-console.log(colors[2]);
 console.log(colors[3]);
+console.log(colors[2]);
+console.log(colors[1]);
+console.log(colors[0]);
